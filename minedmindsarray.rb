4547021e03b3857e72ad5ab9144adef4849ef248
TMerrick55 <tmerrick44@gmail.com>
2017-01-19 12:19:14 -0500

    Add files via upload

--- minedmindsarray.rb
+++ minedmindsarray.rb
@@ -1,6 +1,6 @@
 def create_mined_minds_array()
 	# create an empty array
-	[] mined_minds_array = []
+	mined_minds_array = []
 	100.times do
 		# push an item into the array
 		mined_minds_array << 'array item'
--- testminedmindsarray.rb
+++ testminedmindsarray.rb
@@ -1,9 +1,19 @@
 require 'minitest/autorun'
 require_relative 'minedmindsarray.rb'
+
 class TestMMFunctions < Minitest::Test
+
+	def test_one_equals_one
+		assert_equal(1,1)
+	end
 
 	def test_function_returns_array()
 		results = create_mined_minds_array()
 		assert_equal(Array, results.class)
+	end
+
+	def test_function_returns_array_100
+		results = create_mined_minds_array()
+		assert_equal(100, results.length)
 	end
 end

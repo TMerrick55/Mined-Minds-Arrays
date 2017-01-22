cc615e23a4fe0e06907ab02c53ba06e99c22dd2e
TMerrick55 <tmerrick44@gmail.com>
2017-01-22 11:41:39 -0500

    Add files via upload

--- minedmindsarray.rb
+++ minedmindsarray.rb
@@ -1,10 +1,21 @@
 def create_mined_minds_array()
 	# create an empty array
 	mined_minds_array = []
+
+num = 1
+
 	100.times do
-		# push an item into the array
-		mined_minds_array << 'array item'
+		if num % 5 == 0 && num % 3 == 0
+			mined_minds_array << 'mined minds'
+		elsif num % 5 == 0
+			mined_minds_array << 'minds'
+		elsif num % 3 == 0
+			mined_minds_array << 'mined'
+		else
+			mined_minds_array << num
+		end
+		num = num + 1
+
 	end
-	# return the array
 	mined_minds_array
 end
--- testminedmindsarray.rb
+++ testminedmindsarray.rb
@@ -3,17 +3,32 @@ require_relative 'minedmindsarray.rb'
 
 class TestMMFunctions < Minitest::Test
 
-	def test_one_equals_one
+	def test_one_equals_one()
 		assert_equal(1,1)
 	end
 
-	def test_function_returns_array()
+	def test_function_returns_array_100()
 		results = create_mined_minds_array()
 		assert_equal(Array, results.class)
 	end
 
-	def test_function_returns_array_100
+	def test_function_returns_array_100()
 		results = create_mined_minds_array()
 		assert_equal(100, results.length)
+	end
+
+	def test_function_returns_numbers_divisible_by_3()
+		results = create_mined_minds_array()
+		assert_equal('mined', results[2])
+	end
+
+	def test_function_returns_numbers_divisible_by_5()
+		results = create_mined_minds_array()
+		assert_equal('minds', results[4])
+	end
+
+	def test_function_returns_numbers_divisible_by_3_and_5()
+		results = create_mined_minds_array()
+		assert_equal('mined minds', results[14])
 	end
 end
